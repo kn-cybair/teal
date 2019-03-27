481729216f0d72399b7d2d2e140294d2ac460fe0
szymonzmuds <szymon.zmuda@student.put.poznan.pl>
2019-03-27 14:46:18 +0100

    changing name of folders

---
+++ Zajecia_2/LED_Rezystor/LED_Rezystor.ino
@@ -0,0 +1,28 @@
+/*
+  MIT License 2019
+  ---
+  Arduino Workshops
+  Class #1
+  Program purpose: Blinking LED diode
+  ---
+  @author: Krzysztof Stezala
+  ---
+  Provided by CybAiR Science Club at 
+  Institute of Control, Robotics and Information Engineering of
+  Poznan University of Technology  
+*/
+
+#define LED_PIN 8                     // LED diode pin definition
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  pinMode(LED_PIN, OUTPUT);          // initialize digital pin LED_PIN as an output.
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED_PIN, HIGH);       // turn the LED on (HIGH is the voltage level)
+  delay(2000);                       // wait for a second
+  digitalWrite(LED_PIN, LOW);        // turn the LED off by making the voltage LOW
+  delay(2000);                       // wait for a second
+}
